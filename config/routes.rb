87f1f717042eb4e12c5acf31086d81b9fac15a1c
Tushar <tushrrrocks@gmail.com>
2024-04-13 01:17:01 +0530

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'signup/create'
   resources :records
   resources :artists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
